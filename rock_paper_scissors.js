a5f2e9dbdedc65748eb453e46271cda909f3866a
pstojchevska <60014821+pstojchevska@users.noreply.github.com>
2023-09-03 21:40:10 +0200

    Add starter files

---
+++ rock_paper_scissors.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    let choice;
+    if (computerChoice === 0) {
+        choice = "Rock";
+    } else if (computerChoice === 1) {
+        choice = "Paper";
+    } else if (computerChoice === 2) {
+        choice = "Scissors";
+    }
+    return choice;
+}
